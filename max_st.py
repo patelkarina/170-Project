ef67533124d129ed1d3fe757dece5e81b2084285
Class Account <cs170-aek@imail.EECS.Berkeley.EDU>
2020-05-02 17:20:04 -0700

    mihiras

--- max_st.py
+++ max_st.py
@@ -5,10 +5,10 @@ import os
 
 if __name__ == "__main__":
     output_dir = "outputs"
-    input_dir = "inputs"
+    input_dir = "inputs/large_inputs/large_part9"
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
         G = read_input_file(f"{input_dir}/{input_path}")
         T = solve(G)
         assert is_valid_network(G, T)
-        write_output_file(T, f"{output_dir}/{graph_name}.out")
+        write_output_file(T, f"outputs/{graph_name}.out")
--- solver.py
+++ solver.py
@@ -3,42 +3,26 @@ from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance, average_pairwise_distance_fast
 import sys
 
-
-# def solve(G):
- 
-
-# Start at any vertex v. 
-# Explores all its neighbors and go to the one with the least pairwise 
-# distance. 
-# Continue until we find the whole tree. 
-# Remove the leaves iff it reduces the overall cost 
-# (prevents us from removing something <1). 
-# Repeat for all vertices. Compare the graphs and return 
-# the one with the least cost
-
 def solve(G):
-    unexplored = []
-    start_vertex = list(G.nodes())[0]
-    tree = nx.Graph()
-    tree.add_node(start_vertex)
-    for vertex in G.neighbors(start_vertex):
-    	unexplored.append(vertex)
-    current_node = start_vertex
-    while current_node != None:
-        next_node = choose_best_neighbor(G, tree)
-        current_node = next_node
-        if current_node != None:
-            for vertex in G.neighbors(current_node):
-                if vertex not in unexplored:
-                    if vertex not in tree.nodes():
-                        unexplored.append(vertex)
-            unexplored.remove(current_node)
-    print_tree = remove_leaves(G, tree)
-    #print(tree.nodes())
-    #print(tree.edges())
-    #print(print_tree.nodes())
-    #print(print_tree.edges())
-    return print_tree
+#     unexplored = []
+#     start_vertex = list(G.nodes())[0]
+#     tree = nx.Graph()
+#     tree.add_node(start_vertex)
+#     for vertex in G.neighbors(start_vertex):
+#     	unexplored.append(vertex)
+#     current_node = start_vertex
+#     while current_node != None:
+#         next_node = choose_best_neighbor(G, tree)
+#         current_node = next_node
+#         if current_node != None:
+#             for vertex in G.neighbors(current_node):
+#                 if vertex not in unexplored:
+#                     if vertex not in tree.nodes():
+#                         unexplored.append(vertex)
+#             unexplored.remove(current_node)
+
+    mst = nx.minimum_spanning_tree(G)
+    return remove_leaves(G, mst)
 
 def choose_best_neighbor(G, tree):
     least_pairwise_distance = float('inf')
@@ -48,7 +32,7 @@ def choose_best_neighbor(G, tree):
        for n in list(G.neighbors(t)):
             if n not in tree.nodes():
                 tree.add_edge(t, n)
-                pairwise_distance = average_pairwise_distance(tree)
+                pairwise_distance = average_pairwise_distance_fast(tree)
                 if pairwise_distance < least_pairwise_distance:
                     least_pairwise_vertex = t
                     least_pairwise_neighbor = n
@@ -62,22 +46,14 @@ def remove_leaves(G, tree):
     all_leaves = []
     new_leaves = find_new_leaves(tree, [],  all_leaves)
     for leaf in new_leaves:
-        #print("inremoveleaves")
-        #print(tree.nodes())
-        #print(tree.edges())
         cost_with_leaf = average_pairwise_distance(tree)
-        #neighbor_leaf = tree.neighbors(leaf)
         for neighbor in tree.neighbors(leaf):
             neighbor_leaf = neighbor
         tree.remove_node(leaf) 
         cost_without_leaf = average_pairwise_distance(tree)
         if (nx.is_dominating_set(G, tree.nodes())):
              if cost_with_leaf < cost_without_leaf:
-                 #print("beforeedge")
-                 #print(tree.edges())
                  tree.add_edge(neighbor_leaf, leaf)
-                 #print("afteredge")
-                 #print(tree.edges())
         else:
             tree.add_edge(leaf, neighbor_leaf)
         new_leaves.remove(leaf)
@@ -97,11 +73,11 @@ def find_new_leaves(tree, new_leaves, all_leaves):
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#	assert len(sys.argv) == 2
-#	path = sys.argv[1]
-#	G = read_input_file(path)
-#	T = solve(G)
-#	assert is_valid_network(G, T)
-#	print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-#        write_output_file(T, 'out/test.out')
+#if __name__ == '__main__':
+#    assert len(sys.argv) == 2
+#    path = sys.argv[1]
+#    G = read_input_file(path)
+#    T = solve(G)
+#    assert is_valid_network(G, T)
+#    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+#    write_output_file(T, 'out/test.out')
